c2eea59dc4c7bfff2ae5e0fa5d00aebe06d06c3a
shaman771 <shaman771@gmail.com>
2022-10-30 16:42:04 +0200

    set production root path

--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: 'warning',
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/drum-machine/'
+    : '/'
 })
